c07668865d115782b0ef8c996616987950587cab
kam <kmathes348@students.mchenry.edu>
2019-02-05 17:21:01 -0600

    more changes and additions

--- app/src/main/java/mad155/kmathes/mad155module3englishtometricconverter/MainActivity.java
+++ app/src/main/java/mad155/kmathes/mad155module3englishtometricconverter/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,10 +28,15 @@ public class MainActivity extends AppCompatActivity {
         final Spinner spnrConversion = (Spinner) findViewById(R.id.txtConversion);
         Button btnConvert = (Button) findViewById(R.id.btnConversion);
 
+        //final RadioButton rbUsToMetric = findViewById(R.id.rb1);
+        //final RadioButton rbMetricToUs = findViewById(R.id.rb2);
+
         btnConvert.setOnClickListener(new View.OnClickListener() {
 
             final TextView results = (TextView) findViewById(R.id.txtResult);
 
+            final RadioButton rbUsToMetric = findViewById(R.id.rb1);
+            final RadioButton rbMetricToUs = findViewById(R.id.rb2);
 
             @Override
             public void onClick(View v) {
@@ -44,44 +51,127 @@ public class MainActivity extends AppCompatActivity {
                     //numberToConvert = Integer.parseInt(txtNbr.getText().toString());
                     numberToConvert = Double.parseDouble(txtNbr.getText().toString());
                     double answer = 10;
-                    int convertTypeChoice = 0;
+                    int convertTypeChoice = 1;
+                    String txtConversionUsOrMetric = "";
 
-                    //totalCost = costPerTicket * numberOfTickets;
-                    DecimalFormat formattedNbr = new DecimalFormat("###,###.##");
+                    DecimalFormat formattedNbr = new DecimalFormat("###,###,###.####");
 
                     convertChoice = spnrConversion.getSelectedItem().toString();
 
                     //convertTypeChoice = spnrConversion.getSelectedItemPosition();
 
-                    if(convertChoice.equalsIgnoreCase("inches to centimeters")) {
-                        answer = numberToConvert * 2.54;
-                        convertTypeChoice = 1;
-                    } else if (convertChoice.equalsIgnoreCase("feet to meters")){
-                        convertTypeChoice = 2;
-                    } else if (convertChoice.equalsIgnoreCase("yards to meters")){
-                        convertTypeChoice = 3;
-                    } else if (convertChoice.equalsIgnoreCase("miles to kilometers")){
-                        convertTypeChoice = 4;
-                    } else if (convertChoice.equalsIgnoreCase("fluid ounces to milliliters")){
-                        convertTypeChoice = 5;
-                    } else if (convertChoice.equalsIgnoreCase("quarts to liters")){
-                        convertTypeChoice = 6;
-                    } else if (convertChoice.equalsIgnoreCase("ounces to grams")){
-                        convertTypeChoice = 7;
-                    } else if (convertChoice.equalsIgnoreCase("grams to liters")){
-                        convertTypeChoice = 8;
-                    } else if (convertChoice.equalsIgnoreCase("pounds to kilograms")){
-                        convertTypeChoice = 9;
+                    if (convertChoice.equalsIgnoreCase("inches <--> centimeters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 2.54;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Inches to Centimeters";
+                        } else {
+                            answer = numberToConvert / 2.54;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Centimeters to Inches";
+                        }
+
+                    } else if (convertChoice.equalsIgnoreCase("feet <--> meters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * .3048;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Feet to Meters";
+                        } else {
+                            answer = numberToConvert / .3048;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Meters to Feet";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("yards <--> meters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * .9144;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Yards to Meters";
+                        } else {
+                            answer = numberToConvert / .9144;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Meters to Yards";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("miles <--> kilometers")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 1.609344;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Miles to Kilometers";
+                        } else {
+                            answer = numberToConvert / 1.609344;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Kilometers to Miles";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("fluid ounces <--> milliliters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 29.57353;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Fluid Ounces to Milliliters";
+                        } else {
+                            answer = numberToConvert / 29.57353;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Milliliters to Fluid Ounces";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("quarts <--> liters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * .946352946;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Quarts to Liters";
+                        } else {
+                            answer = numberToConvert / .946352946;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Liters to Quarts";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("gallons <--> liters")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 3.785411784;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Gallons to Liters";
+                        } else {
+                            answer = numberToConvert / 3.785411784;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Liters to Gallons";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("ounces <--> grams")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 28.34952;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Ounces to Grams";
+                        } else {
+                            answer = numberToConvert / 28.34952;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Grams to Ounces";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("pounds <--> grams")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * 453.592;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Pounds to Grams";
+                        } else {
+                            answer = numberToConvert / 453.592;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Grams to Pounds";
+                        }
+                    } else if (convertChoice.equalsIgnoreCase("pounds <--> kilograms")) {
+                        if (rbUsToMetric.isChecked()) {
+                            answer = numberToConvert * .45359237;
+                            txtConversionUsOrMetric = "Converting U.S. to Metric";
+                            convertChoice = "Pounds to Kilograms";
+                        } else {
+                            answer = numberToConvert / .45359237;
+                            txtConversionUsOrMetric = "Converting Metric to U.S.";
+                            convertChoice = "Kilograms to Pounds";
+                        }
 
                     } else {
                         convertChoice = "other choice";
                     }
 
-                    results.setText("Conversion of " + numberToConvert + "\n(" + convertChoice + ") is: " +
-                            formattedNbr.format(answer) + " convertTypeChoice = " + convertTypeChoice);
-                }
+                    results.setText("Number (input) being converted: " + numberToConvert + "\n" +
+                            txtConversionUsOrMetric + " \n" + convertChoice +
+                            "\n*** Answer:  " + formattedNbr.format(answer) + " ***");
 
+                }
             }
         });
     }
-}
+ }
